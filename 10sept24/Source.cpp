26b0165c13b1532c16e4d041e41f473e23564c43
acm29636 <acm29636@HA-STEMCART-53>
2024-09-10 17:42:56 -0400

    Add project files.

---
+++ 10sept24/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int width, length;
+	cout << "What's the length of the rectangle?";
+	cin >> length;
+	cout << "What's the width of the rectangle?";
+	cin >> width;
+	int area;
+	area = length * width;
+		cout << "The area of your rectangle is " << area << ".";
+		return 0;
+
+}
+
+//calculate area of rectangle
+//input length and width
+//output area of rectangle
+//area = l * w
